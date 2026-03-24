f2ca010fc40b70cc42e26aff886c63fe2ea97627
Melissa Gomez <melissagomez3024@gmail.com>
2026-03-24 06:04:01 +0100

    feat: configure Main class as application entry point

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,28 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import org.example.view.MovieView;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        MovieView vista = new MovieView();
+
+        System.out.println("🚀 Iniciando sistema de Videoclub...");
+        package org.example;
+
+import org.example.view.MovieView;
+
+        public class Main {
+            public static void main(String[] args) {
+                // 1. Creamos el objeto de la Vista
+                MovieView vista = new MovieView();
+
+                // 2. Imprimimos un mensaje de bienvenida
+                System.out.println("🚀 Iniciando sistema de Videoclub...");
+
+                // 3. Llamamos al método que tiene el bucle y el Scanner
+                vista.mostrarMenu();
+            }
         }
+        vista.mostrarMenu();
     }
 }
