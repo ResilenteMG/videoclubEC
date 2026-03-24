a74172076a58af40ffb4cb9e97c35e0cccb07acd
Melissa Gomez <melissagomez3024@gmail.com>
2026-03-24 06:18:32 +0100

    feat: configure Main class as application entry point

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,28 +1,14 @@
-package org.example;
+package org.example; // 1. Primero el paquete
 
-import org.example.view.MovieView;
+import org.example.view.MovieView; // 2. Luego las importaciones
+
 
 public class Main {
     public static void main(String[] args) {
         MovieView vista = new MovieView();
 
         System.out.println("🚀 Iniciando sistema de Videoclub...");
-        package org.example;
 
-import org.example.view.MovieView;
-
-        public class Main {
-            public static void main(String[] args) {
-                // 1. Creamos el objeto de la Vista
-                MovieView vista = new MovieView();
-
-                // 2. Imprimimos un mensaje de bienvenida
-                System.out.println("🚀 Iniciando sistema de Videoclub...");
-
-                // 3. Llamamos al método que tiene el bucle y el Scanner
-                vista.mostrarMenu();
-            }
-        }
         vista.mostrarMenu();
     }
 }
